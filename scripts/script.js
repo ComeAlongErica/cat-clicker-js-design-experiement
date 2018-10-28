2f0e8d1613315a573c753f8c1b7f04982c7022e4
ComeAlongErica <ericaraephoto@gmail.com>
2018-10-28 07:31:50 -0500

    Meet new requirement. Have two cat images, with names, and seperate counters.

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,23 @@
 "use strict";
 $(document).ready(() => {
 
-    let counter = 0;  
+    let counter1 = 0;  
+    let counter2 = 0;  
 
-    $(document).on("click", ".cat-img", (event) => {
-        counter= counter + 1;  
-        $(".counter").html("");
-        $(".counter").html(`<p>You've clicked ${counter} times.</p>`);
+
+    $(document).on("click", ".cat-img-1", (event) => {
+        counter1 = counter1 + 1;   
+        
+        $(".counter-cat-1").html("");
+        $(".counter-cat-1").html(`<p>You've clicked ${counter1} times.</p>`);
     });
+
+
+    $(document).on("click", ".cat-img-2", (event) => {
+        counter2 = counter2 + 1;   
+        
+        $(".counter-cat-2").html("");
+        $(".counter-cat-2").html(`<p>You've clicked ${counter2} times.</p>`);
+    });
+
 });
