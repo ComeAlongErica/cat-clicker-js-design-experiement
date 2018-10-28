f370539b5e8d3cc98b6b03022edb141b566f4a2a
ComeAlongErica <ericaraephoto@gmail.com>
2018-10-28 11:37:03 -0500

    created IIFE to add event listeners

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,59 @@
 "use strict";
 $(document).ready(() => {
+    let catArray = [
+        {
+            name: "Jayne Cobb",
+            url: "images/cat-photo-1.jpg"
+        },
+        {
+            name: "Malcolm Reynolds",
+            url: "images/cat-photo-2.jpg"
+        },
+        {
+            name: "Kaylee Frye",
+            url: "images/cat-photo-3.jpeg"
+        },
+        {
+            name: "Zoe Washburne",
+            url: "images/cat-photo-4.jpeg"
+        },
+        {
+            name: "River Tam",
+            url: "images/cat-photo-5.jpg"
+        }
+    ];
 
-    let counter1 = 0;  
-    let counter2 = 0;  
+    const pageSetup = () => {
+        for (let i = 0; i < catArray.length; i++) {
+            let cat = catArray[i]; // specific cat we are on
+            let counter = 0;
 
+            let list = document.createElement('p'); //creates element
+            list.textContent = cat.name; // give element content
 
-    $(document).on("click", ".cat-img-1", (event) => {
-        counter1 = counter1 + 1;   
-        
-        $(".counter-cat-1").html("");
-        $(".counter-cat-1").html(`<p>You've clicked ${counter1} times.</p>`);
-    });
+            //creates event listener for element
+            list.addEventListener("click", (function (catCopy) {
+                return function () {
+                    //increases counter
+                    counter = counter + 1;
 
+                    $(".display-cat").html(""); // clears container
+                    $(".counter-cat").remove() //clears previous count
 
-    $(document).on("click", ".cat-img-2", (event) => {
-        counter2 = counter2 + 1;   
-        
-        $(".counter-cat-2").html("");
-        $(".counter-cat-2").html(`<p>You've clicked ${counter2} times.</p>`);
-    });
+                    $(".display-cat").append(
+                        `<p class="cat-name">${catCopy.name}</p>
+                         <img src="${catCopy.url}">
+                         <div class="counter-cat">${catCopy.name} has been clicked ${counter} times.</div>
+                         `);
+                };
+            })(cat));
+
+            $(".cat-name-list").append(list);
+        };
+
+    };
+
+    pageSetup();
 
 });
+
